3a525c0fe5a8692a58a6f92ec203f29a6f5ddc83
hasan <hasan1993mur@gmail.com>
2021-03-15 14:52:37 +0500

    разбил по пакетам

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import task3.MathBox;
+
 import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
+    private static Object Number;
+
     public static void main(String[] args) {
 
+        MathBox mathBox1=new MathBox(null);
+        int hCode;
+        hCode=mathBox1.hashCode();
+        System.out.println(hCode);
+        System.out.println(mathBox1.mathbox());
+        System.out.println(mathBox1.summer());
+        System.out.println(mathBox1.splitter(2));
+       System.out.println(mathBox1.input());
 
         }
 }
